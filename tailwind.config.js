747a262414001ead9f4b894a5e6bb2b8ac855a22
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-02-20 11:50:18 +0530

    added index.html

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      height:{
+        rajat: "3.94rem",
+        81: "325.96px"
+      },
+      fontSize:{
+        singh : "41px",
+        pa : "17px",
+      },
+      contrast:{
+       25:".60",
+      },
+      margin:{
+         25: "100px",
+         18: "74px",
+         20:"84px",
+         
+      },
+      padding:{
+        86:"360px",
+        65:"274px",
+       68:"278px",
+       50:"200px",
+      },
+    },
+  },
+  plugins: [],
+}
+
